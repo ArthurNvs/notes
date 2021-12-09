b7cd4e082a001b41fa53a4781e86d659b25ec843
Arthur Neves <arthurnvsm@gmail.com>
2021-12-09 15:05:45 -0300

    Saving data locally

--- Notes WatchKit Extension/View/ContentView.swift
+++ Notes WatchKit Extension/View/ContentView.swift
@@ -12,8 +12,22 @@ struct ContentView: View {
   @State private var notes: [Note] = [Note]()
   @State private var text: String = ""
   
+  func getDocumentDirectory() -> URL {
+    let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    return path[0]
+  }
+  
   func save() {
-    dump(notes)
+    do {
+      // 1. Convert notes array to data using JSONEncoder
+      let data = try JSONEncoder().encode(notes)
+      // 2. Create new URL to save file using getDocumentDirectory
+      let url = getDocumentDirectory().appendingPathComponent("notes")
+      // 3. Write data to the given URL
+      try data.write(to: url)
+    } catch {
+      print("saving data failed!")
+    }
   }
   
   var body: some View {
@@ -23,12 +37,9 @@ struct ContentView: View {
         
         Button(action: {
           guard !text.isEmpty else { return }
-          
           let note = Note(id: UUID(), text: text)
           notes.append(note)
-          
           text = ""
-          
           save()
         }) {
           Image(systemName: "plus.circle")
